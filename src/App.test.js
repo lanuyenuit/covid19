b71e5e39b26926fb602978204a76ee3adf825920
uyen <lanuyen.uit@gmail.com>
2021-03-14 23:23:53 +0700

    added basic unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Render App', () => {
+  it('should render App', async () => {
+    const { findByText } = render(<App />)
+    expect(await findByText('COVID19 Information')).toBeInTheDocument()
+  })
+
+  it('should render worldwide statistics', async () => {
+    const { findByText } = render(<App />)
+    expect(await findByText('Hide Worldwide Summary')).toBeInTheDocument()
+    expect(await findByText('Country')).toBeInTheDocument()
+    expect(await findByText('New Cases')).toBeInTheDocument()
+    expect(await findByText('New Deaths')).toBeInTheDocument()
+    expect(await findByText('Total Deaths')).toBeInTheDocument()
+    expect(await findByText('New Recovered')).toBeInTheDocument()
+    expect(await findByText('Total Recovered')).toBeInTheDocument()
+  })
+
+  it('should hide worldwide statistics when clicks on Hide Worldwide Summary', async () => {
+    const { findByText, queryByText } = render(<App />)
+
+    const toggleButton = await findByText('Hide Worldwide Summary')
+    fireEvent.click(toggleButton)
+    expect(await findByText('Show Worldwide Summary')).toBeInTheDocument()
+    expect(queryByText('Country')).not.toBeInTheDocument()
+    expect(queryByText('New Cases')).not.toBeInTheDocument()
+    expect(queryByText('New Deaths')).not.toBeInTheDocument()
+    expect(queryByText('Total Deaths')).not.toBeInTheDocument()
+    expect(queryByText('New Recovered')).not.toBeInTheDocument()
+    expect(queryByText('Total Recovered')).not.toBeInTheDocument()
+  })
+})
